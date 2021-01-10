9b64079b149fbe63c60a7b381f84a86ef9ff2a0b
DimaChekashov <foxdima99@gmail.com>
2021-01-10 17:31:49 +0300

    add output block to border-radius

--- js/border-radius.js
+++ js/border-radius.js
@@ -1,5 +1,6 @@
 const inputs = document.querySelectorAll(".border-radius__input"),
-    box = document.querySelector(".border-radius-result");
+    box = document.querySelector(".border-radius-result"),
+    output = document.querySelector(".border-radius-output");
 
 inputs.forEach((item) => {
     console.log(item.getAttribute("data-toggle"));
@@ -17,5 +18,36 @@ inputs.forEach((item) => {
         } else if (item.getAttribute("data-toggle") === "border-width") {
             box.style.border = item.value + "px solid #000000";
         }
+        output.innerHTML = "";
+        renderOutput();
     });
 });
+
+function renderOutput() {
+    output.innerHTML += `
+        border:  ${
+            box.style.borderWidth ? box.style.borderWidth : "0px"
+        } solid #000000;<br>
+        border-top-left-radius: ${
+            box.style.borderTopLeftRadius
+                ? box.style.borderTopLeftRadius
+                : "10px"
+        };<br>
+        border-top-right-radius: ${
+            box.style.borderTopRightRadius
+                ? box.style.borderTopRightRadius
+                : "10px"
+        };<br>
+        border-bottom-left-radius: ${
+            box.style.borderBottomLeftRadius
+                ? box.style.borderBottomLeftRadius
+                : "10px"
+        };<br>
+        border-bottom-right-radius: ${
+            box.style.borderBottomRightRadius
+                ? box.style.borderBottomRightRadius
+                : "10px"
+        };<br>
+    `;
+}
+renderOutput();
