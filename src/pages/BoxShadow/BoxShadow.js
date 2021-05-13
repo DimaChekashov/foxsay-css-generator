8e3f780b19ca55657b48e6e1333d3ed0919abfed
DimaChekashov <foxdima99@gmail.com>
2021-05-13 22:22:16 +0300

    rewrite BoxShadow with hooks

--- src/pages/BoxShadow/BoxShadow.js
+++ src/pages/BoxShadow/BoxShadow.js
@@ -1,147 +1,119 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./BoxShadow.css";
 import { onCopyText } from "../../utils/utils";
 
-export default class BoxShadow extends Component {
-    inputs = [
+function BoxShadow(props) {
+    const [horizontalLength, setHorizontalLength] = useState(10);
+    const [verticalLength, setVerticalLength] = useState(10);
+    const [blurRadius, setBlurRadius] = useState(10);
+    const [spreadRadius, setSpreadRadius] = useState(10);
+    const [opacityBox, setOpacityBox] = useState(0.75);
+
+    const inputs = [
         {
             label: "Horizontal Length",
-            name: "horizontalLength",
-            eventKeyName: (name, key) => {
-                this.handleKeyDown(name, key);
+            name: horizontalLength,
+            setValue: setHorizontalLength,
+            eventKeyName: (setValue, key) => {
+                handleKeyDown(setValue, key);
             },
         },
         {
             label: "Vertical Length",
-            name: "verticalLength",
-            eventKeyName: (name, key) => {
-                this.handleKeyDown(name, key);
+            name: verticalLength,
+            setValue: setVerticalLength,
+            eventKeyName: (setValue, key) => {
+                handleKeyDown(setValue, key);
             },
         },
         {
             label: "Blur Radius",
-            name: "blurRadius",
-            eventKeyName: (name, key) => {
-                this.handleKeyDown(name, key);
+            name: blurRadius,
+            setValue: setBlurRadius,
+            eventKeyName: (setValue, key) => {
+                handleKeyDown(setValue, key);
             },
         },
         {
             label: "Spread Radius",
-            name: "spreadRadius",
-            eventKeyName: (name, key) => {
-                this.handleKeyDown(name, key);
+            name: spreadRadius,
+            setValue: setSpreadRadius,
+            eventKeyName: (setValue, key) => {
+                handleKeyDown(setValue, key);
             },
         },
         {
             label: "Opacity Box",
-            name: "opacityBox",
-            eventKeyName: (name, key) => {
-                this.handleKeyDownOpacity(name, key);
+            name: opacityBox,
+            setValue: setOpacityBox,
+            eventKeyName: (setValue, key) => {
+                handleKeyDownOpacity(setValue, key);
             },
         },
     ];
 
-    constructor() {
-        super();
-
-        this.state = {
-            horizontalLength: 10,
-            verticalLength: 10,
-            blurRadius: 10,
-            spreadRadius: 10,
-            opacityBox: 0.75,
-        };
-
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleKeyDown = this.handleKeyDown.bind(this);
-        this.handleKeyDownOpacity = this.handleKeyDownOpacity.bind(this);
-    }
-
-    handleInputChange(name, val) {
+    function handleInputChange(setValue, val) {
         if (!Number.isInteger(+val)) return;
-        this.setState({
-            [name]: +val || 0,
-        });
+        setValue(+val || 0);
     }
-    handleKeyDown(name, key) {
+    function handleKeyDown(setValue, key) {
         if (key === "ArrowUp") {
-            this.setState({
-                [name]: this.state[name] >= 100 ? 100 : this.state[name] + 1,
-            });
+            setValue((prevCount) => (prevCount >= 100 ? 100 : prevCount + 1));
         } else if (key === "ArrowDown") {
-            this.setState({
-                [name]: this.state[name] > -100 ? this.state[name] - 1 : -100,
-            });
+            setValue((prevCount) => (prevCount > -100 ? prevCount - 1 : -100));
         }
     }
-    handleKeyDownOpacity(name, key) {
+    function handleKeyDownOpacity(setValue, key) {
         if (key === "ArrowUp") {
-            this.setState({
-                [name]:
-                    this.state[name] >= 1
-                        ? 1
-                        : +(this.state[name] + 0.01).toFixed(2),
-            });
+            setValue((prevCount) =>
+                prevCount >= 1 ? 1 : +(prevCount + 0.01).toFixed(2)
+            );
         } else if (key === "ArrowDown") {
-            this.setState({
-                [name]:
-                    this.state[name] > 0
-                        ? (this.state[name] - 0.01).toFixed(2)
-                        : 0,
-            });
+            setValue((prevCount) =>
+                prevCount > 0 ? (prevCount - 0.01).toFixed(2) : 0
+            );
         }
     }
 
-    render() {
-        const {
-            horizontalLength,
-            verticalLength,
-            blurRadius,
-            spreadRadius,
-            opacityBox,
-        } = this.state;
-
-        return (
-            <>
-                <div className="generator box-shadow">
-                    <div className="generator__inputs">
-                        {this.inputs.map(({ label, name, eventKeyName }) => (
-                            <div className="input-group" key={name}>
-                                <h4>{label}</h4>
-                                <input
-                                    className="input-group__input"
-                                    type="text"
-                                    value={this.state[name]}
-                                    onChange={(e) =>
-                                        this.handleInputChange(
-                                            name,
-                                            e.target.value
-                                        )
-                                    }
-                                    onKeyDown={(e) => eventKeyName(name, e.key)}
-                                />
-                            </div>
-                        ))}
-                    </div>
-                    <div className="generator__result-wrapper">
-                        <div
-                            className="box-shadow__result"
-                            style={{
-                                boxShadow: `${horizontalLength}px ${verticalLength}px ${blurRadius}px ${spreadRadius}px rgba(0, 0, 0, ${opacityBox})`,
-                            }}
-                        ></div>
-                    </div>
+    return (
+        <>
+            <div className="generator box-shadow">
+                <div className="generator__inputs">
+                    {inputs.map(({ label, name, setValue, eventKeyName }) => (
+                        <div className="input-group" key={label}>
+                            <h4>{label}</h4>
+                            <input
+                                className="input-group__input"
+                                type="text"
+                                value={name}
+                                onChange={(e) =>
+                                    handleInputChange(setValue, e.target.value)
+                                }
+                                onKeyDown={(e) => eventKeyName(setValue, e.key)}
+                            />
+                        </div>
+                    ))}
                 </div>
-                <div className="generator__output">
-                    {`box-shadow: ${horizontalLength}px ${verticalLength}px ${blurRadius}px ${spreadRadius}px rgba(0, 0, 0, ${opacityBox})`}
+                <div className="generator__result-wrapper">
+                    <div
+                        className="box-shadow__result"
+                        style={{
+                            boxShadow: `${horizontalLength}px ${verticalLength}px ${blurRadius}px ${spreadRadius}px rgba(0, 0, 0, ${opacityBox})`,
+                        }}
+                    ></div>
                 </div>
-                <button
-                    className="btn-copy"
-                    onClick={() => onCopyText(".generator__output")}
-                >
-                    Скопировать текст
-                </button>
-            </>
-        );
-    }
+            </div>
+            <div className="generator__output">
+                {`box-shadow: ${horizontalLength}px ${verticalLength}px ${blurRadius}px ${spreadRadius}px rgba(0, 0, 0, ${opacityBox})`}
+            </div>
+            <button
+                className="btn-copy"
+                onClick={() => onCopyText(".generator__output")}
+            >
+                Скопировать текст
+            </button>
+        </>
+    );
 }
+
+export default BoxShadow;
