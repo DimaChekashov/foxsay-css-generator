fb565ed86e3cfbe2b03009c0fb5185cb9a7b3910
DimaChekashov <foxdima99@gmail.com>
2021-05-14 01:07:56 +0300

    add main navigation

---
+++ src/component/Navigation/Navigation.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./Navigation.scss";
+import { NavLink } from "react-router-dom";
+
+function Navigation() {
+    return (
+        <div className="navigation">
+            <NavLink
+                className="navigation-btn"
+                to="/box-shadow"
+            >BoxShadow</NavLink>
+            <NavLink
+                className="navigation-btn"
+                to="/border-radius"
+            >BorderRadius</NavLink>
+        </div>
+    );
+}
+
+export default Navigation;
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Navigation from "../../component/Navigation/Navigation";
+
 import "./MainPage.css";
 
 function MainPage() {
     return (
         <div className="main">
-            <h1>Главная страница</h1>
+            <h1 className="title">FoxSay CSS Generator</h1>
+            <h3 className="subtitle">Генератор CSS свойств</h3>
+            <Navigation />
         </div>
     );
 }
